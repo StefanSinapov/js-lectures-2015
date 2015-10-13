c779f8ab16dfb5defea2dc74a458a09b5d9324a1
Stefan Sinapov <stefan.sinapov@gmail.com>
2015-10-13 23:34:12 +0300

    task02.js

--- tasks/01/task02.js
+++ tasks/01/task02.js
@@ -1,7 +1,21 @@
-import { fib } from 'task01'
-
 function phiEstimation(n) {
 	return fib(n) / fib(n - 1);
 }
+
+function fib(n) {
+
+	var first = 0;
+	var second = 1;
+	var temp;
+
+	while (n > 0) {
+		temp = first;
+		first = second;
+		second += temp;
+		n--;
+	}
+
+	return first;
+};
 
 console.log(phiEstimation(55));
